b4f97e29e2473868fc8d50e0434ec26fe6f826d9
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-03-26 11:08:38 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000 // Specify the port you want to use here
+  }
 })
